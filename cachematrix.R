3a5f71824144c315d7d445b9c8ac82158c644af7
Mash Zahid <mzahid@alineahealth.com>
2014-12-21 16:11:45 -0500

    assignment completed
    Pithy comments added

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,49 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## makeCacheMatrix, frankly, is similar to the provided example code except that the set() function
+## is removed because it is not used by nor required by cacheSolve
 
+makeCacheMatrix <- function(x = matrix()) {
+    
+    m <- NULL
+    
+    get <- function() { x }
+    
+    setInvMatrix <- function(matrix)  { m <<- matrix }
+    
+    getInvMatrix <- function() { m }
+    
+    list(get = get,
+         setInvMatrix = setInvMatrix,
+         getInvMatrix = getInvMatrix)
+
+    
 }
 
 
 ## Write a short comment describing this function
 
+## if anyone has spent time working on this problem successfully, then what more can I say about this
+## cacheSolve function... it is prosaic and straightforward. I wish I was already expert enough to
+## write some sexy R code but that is still a long way ahead. 
+
+## Maybe I'll just add that I like lots of white space alongside reasonable indenting in my code. And good humorous comments with proper grammar and punctuation.
+
+
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    
+    m <- x$getInvMatrix()
+    
+    if(!is.null(m)) {
+        
+        message("getting cached matrix")
+        return(m)
+    }
+    
+    input <- x$get()
+    m <- solve(input, ...)
+    x$setInvMatrix(m)
+    m
 }
